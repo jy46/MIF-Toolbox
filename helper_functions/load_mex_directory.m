871c77bf08a672ba41b3b7ae297befb1103b82e9
jy46 <45184853+jy46@users.noreply.github.com>
2020-06-05 11:02:46 -0500

    Update load_mex_directory.m

--- helper_functions/load_mex_directory.m
+++ helper_functions/load_mex_directory.m
@@ -2,7 +2,7 @@
 % Script to load directories for MIF
 % The folder for mex files is loaded based on which OS is being used
 %
-% Copyright (C) 2020 Joseph Young
+% Copyright (C) 2020 Joseph Young - see GPLv2_note.txt for full notice
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ismac
     addpath tstool_files/mex_mac/
